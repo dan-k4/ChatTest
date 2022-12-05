1dc95f4af01dec29e098c97fe5c71831cb13e395
DanK04 <kawazudan0430@gmail.com>
2022-12-05 21:31:15 +0900

    first commit

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use App\Models\Chat;
+
+class ChatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        // ユーザーIDをセッションに登録
+        $user_identifier = $request->session()->get('user_identifier', Str::random(20));
+        session(['user_identifier' => $user_identifier]);
+
+        // ユーザー名を変数に登録(デフォルト値:Guest)
+        $user_name = $request->session()->get('user_name', 'Guest');
+
+        // データベースの件数を取得
+        $length = Chat::all()->count();
+
+        // 表示する件数を代入
+        $display = 5;
+
+        $chats = Chat::offset($length-$display)->limit($display)->get();
+        return view('chat/index', compact('chats', 'user_identifier', 'user_name'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // ユーザ０名をフォールから取得してセッションに登録
+        session(['user_name' => $request->user_name]);
+
+        $chat = new Chat;
+        $form = $request->all();
+        $chat->fill($form)->save();
+        return redirect('/chat');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Models/Chat.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Chat extends Model
+{
+    use HasFactory;
+
+    protected $fillable =[
+        'user_name',
+        'user_identifier',
+        'message'
+    ];
+
+    public function scopeGetData($query)
+    {
+        return $this->created_at . ' ＠' . $this->user_name . ' ' . $this->message;
+    }
+}
